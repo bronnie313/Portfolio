46c4f6d9865575f6a4c4c2e8a079a0a2196a687b
Bisaso Ronald <bronnie313@gmail.com>
2023-02-07 15:01:52 +0300

    correct eslint errors in Js and stylint errors in css

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active")
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active')
 })
